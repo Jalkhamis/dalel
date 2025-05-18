e34a2e65b0518b18c2afddba5440ea6e1b211be9
JANA <janaalkhamis7@gmail.com>
2025-05-18 18:31:55 +0300

    frist commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const dalel ());
+}
+class dalel extends StatelessWidget {
+  const dalel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
